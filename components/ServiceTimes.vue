c27d30ffcbb2cef20aa0720b37ea401833fb75c1
sergix <sergix@outlook.com>
2023-07-19 12:33:03 -0400

    fix: service times mobile styling

--- components/ServiceTimes.vue
+++ components/ServiceTimes.vue
@@ -1,18 +1,18 @@
 <template>
   <section class="service-times mt-8">
     <h2>Service Times</h2>
-    <div class="ml-8 flex flex-row">
+    <div class="md:ml-8 flex flex-row">
       <h4 class="w-1/3 text-right">Sunday</h4>
       <div class="flex flex-col ml-4">
-        <p v-for="service in sundayServices" :key="service.name">
+        <p class="mb-1" v-for="service in sundayServices" :key="service.name">
           {{ service.name }} — {{ service.time }}
         </p>
       </div>
     </div>
-    <div class="ml-8 flex flex-row">
+    <div class="md:ml-8 flex flex-row">
       <h4 class="w-1/3 text-right">Wednesday</h4>
       <div class="flex flex-col ml-4">
-        <p v-for="service in wednesdayServices" :key="service.name">
+        <p class="mb-2" v-for="service in wednesdayServices" :key="service.name">
           {{ service.name }} — {{ service.time }}
         </p>
       </div>
